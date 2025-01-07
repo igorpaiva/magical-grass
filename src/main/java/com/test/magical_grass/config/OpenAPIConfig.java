2e92a2cd37b47dab44bf75f7451eb289e8c9ab13
Igor Paiva <igor.araujo.ee@gmail.com>
2025-01-07 23:06:37 +0100

    Add openapi to the project

---
+++ src/main/java/com/test/magical_grass/config/OpenAPIConfig.java
@@ -0,0 +1,22 @@
+package com.test.magical_grass.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPIConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Magical Grass")
+                        .version("1.0")
+                        .description("Magical Grass")
+                        .termsOfService("http://magical-grass.com")
+                        .license(new License().name("Apache 2.0").url("http://www.apache.org/licenses/LICENSE-2.0")));
+    }
+}
